a3e27e7a2b1d3f936b8266467d61cbbc89ecbd93
happy <gillharpreetsingh211@gmail.com>
2025-05-15 14:05:30 +0530

    update readme ✅

--- script/GenerateInput.s.sol
+++ script/GenerateInput.s.sol
@@ -17,7 +17,7 @@ contract GenerateInput is Script {
         types[0] = "address";
         types[1] = "uint";
         whitelist[0] = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D";
-        whitelist[1] = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+        whitelist[1] = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
         whitelist[2] = "0x2ea3970Ed82D5b30be821FAAD4a731D35964F7dd";
         whitelist[3] = "0xf6dBa02C01AF48Cf926579F77C9f874Ca640D91D";
         count = whitelist.length;
